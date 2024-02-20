9886197c0c069575a334de3bc6a01019b0b4ebec
vchho <12765624+vchho@users.noreply.github.com>
2024-02-19 22:15:12 -0800

    added components and constants

---
+++ frontend/src/components/combobox.tsx
@@ -0,0 +1,132 @@
+import * as React from "react"
+import { Check, ChevronsUpDown } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+
+const frameworks = [
+  {
+    value: "next.js",
+    label: "Next.js",
+  },
+  {
+    value: "sveltekit",
+    label: "SvelteKit",
+  },
+  {
+    value: "nuxt.js",
+    label: "Nuxt.js",
+  },
+  {
+    value: "remix",
+    label: "Remix",
+  },
+  {
+    value: "astro",
+    label: "Astro",
+  },
+]
+
+export const GAMES: any[] = [
+  {
+    value: '1',
+    text: 'Red, Blue and Yellow',
+    key: '1',
+  },
+  {
+    value: '2',
+    text: 'Gold, Silver and Crystal',
+    key: '2',
+  },
+  {
+    value: '3',
+    text: 'Ruby, Sapphire and Emerald',
+    key: '3',
+  },
+  {
+    value: '4',
+    text: 'FireRed and LeafGreen',
+    key: '4',
+  },
+  {
+    value: '5',
+    text: 'Diamond, Pearl and Platinum',
+    key: '5',
+  },
+  {
+    value: '6',
+    text: 'HeartGold and SoulSilver',
+    key: '6',
+  },
+  {
+    value: '7',
+    text: 'Black and White',
+    key: '7',
+  },
+  {
+    value: '8',
+    text: 'Black 2 and White 2',
+    key: '8',
+  },
+];
+
+export function ComboboxDemo() {
+  const [open, setOpen] = React.useState(false)
+  const [value, setValue] = React.useState("")
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-[200px] justify-between"
+        >
+          {value
+            ? frameworks.find((framework) => framework.value === value)?.label
+            : "Select framework..."}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandInput placeholder="Search framework..." />
+          <CommandEmpty>No game found.</CommandEmpty>
+          <CommandGroup>
+            {frameworks.map((framework) => (
+              <CommandItem
+                key={framework.value}
+                value={framework.value}
+                onSelect={(currentValue) => {
+                  setValue(currentValue === value ? "" : currentValue)
+                  setOpen(false)
+                }}
+              >
+                <Check
+                  className={cn(
+                    "mr-2 h-4 w-4",
+                    value === framework.value ? "opacity-100" : "opacity-0"
+                  )}
+                />
+                {framework.label}
+              </CommandItem>
+            ))}
+          </CommandGroup>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  )
+}
