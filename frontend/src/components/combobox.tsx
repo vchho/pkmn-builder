52bfdd136f3a4d9dd108f8e95912bc0884d5b870
vchho <12765624+vchho@users.noreply.github.com>
2024-03-20 23:50:08 -0700

    added games.ts

--- frontend/src/components/combobox.tsx
+++ frontend/src/components/combobox.tsx
@@ -1,20 +1,20 @@
-import * as React from "react"
-import { Check, ChevronsUpDown } from "lucide-react"
+import * as React from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 import {
   Command,
   CommandEmpty,
   CommandGroup,
   CommandInput,
   CommandItem,
-} from "@/components/ui/command"
+} from "@/components/ui/command";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
+} from "@/components/ui/popover";
 
 const frameworks = [
   {
@@ -37,54 +37,11 @@ const frameworks = [
     value: "astro",
     label: "Astro",
   },
-]
-
-export const GAMES: { value: string, text: string, key: string }[] = [
-  {
-    value: 'rby',
-    text: 'Red, Blue and Yellow',
-    key: '1',
-  },
-  {
-    value: 'gsc',
-    text: 'Gold, Silver and Crystal',
-    key: '2',
-  },
-  {
-    value: 'rse',
-    text: 'Ruby, Sapphire and Emerald',
-    key: '3',
-  },
-  {
-    value: 'frlg',
-    text: 'FireRed and LeafGreen',
-    key: '4',
-  },
-  {
-    value: 'dpp',
-    text: 'Diamond, Pearl and Platinum',
-    key: '5',
-  },
-  {
-    value: 'hgss',
-    text: 'HeartGold and SoulSilver',
-    key: '6',
-  },
-  {
-    value: 'bw',
-    text: 'Black and White',
-    key: '7',
-  },
-  {
-    value: 'bw2',
-    text: 'Black 2 and White 2',
-    key: '8',
-  },
 ];
 
 export function ComboboxDemo() {
-  const [open, setOpen] = React.useState(false)
-  const [value, setValue] = React.useState("")
+  const [open, setOpen] = React.useState(false);
+  const [value, setValue] = React.useState("");
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
@@ -96,7 +53,7 @@ export function ComboboxDemo() {
           className="w-[300px] justify-between"
         >
           {value
-            ? GAMES.find((framework) => framework.text === value)?.text
+            ? frameworks.find((framework) => framework.label === value)?.label
             : "Select framework..."}
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
@@ -106,28 +63,28 @@ export function ComboboxDemo() {
           <CommandInput placeholder="Search framework..." />
           <CommandEmpty>No game found.</CommandEmpty>
           <CommandGroup>
-            {GAMES.map((framework) => (
+            {frameworks.map((framework) => (
               <CommandItem
                 key={framework.value}
-                value={framework.text}
+                value={framework.label}
                 onSelect={(currentValue) => {
-                  console.log(currentValue)
-                  setValue(currentValue === value ? "" : framework.text)
-                  setOpen(false)
+                  console.log(currentValue);
+                  setValue(currentValue === value ? "" : framework.label);
+                  setOpen(false);
                 }}
               >
                 <Check
                   className={cn(
                     "mr-2 h-4 w-4",
-                    value === framework.text ? "opacity-100" : "opacity-0"
+                    value === framework.label ? "opacity-100" : "opacity-0",
                   )}
                 />
-                {framework.text}
+                {framework.label}
               </CommandItem>
             ))}
           </CommandGroup>
         </Command>
       </PopoverContent>
     </Popover>
-  )
+  );
 }
---
+++ frontend/src/constants/games.ts
@@ -0,0 +1,55 @@
+export const GAMES: {
+  value: string;
+  text: string;
+  key: string;
+  generation: number;
+}[] = [
+  {
+    value: "rby",
+    text: "Red, Blue and Yellow",
+    key: "1",
+    generation: 1,
+  },
+  {
+    value: "gsc",
+    text: "Gold, Silver and Crystal",
+    key: "2",
+    generation: 2,
+  },
+  {
+    value: "rse",
+    text: "Ruby, Sapphire and Emerald",
+    key: "3",
+    generation: 3,
+  },
+  {
+    value: "frlg",
+    text: "FireRed and LeafGreen",
+    key: "4",
+    generation: 3,
+  },
+  {
+    value: "dpp",
+    text: "Diamond, Pearl and Platinum",
+    key: "5rgrg",
+    generation: 4,
+  },
+  {
+    value: "hgss",
+    text: "HeartGold and SoulSilver",
+    key: "6",
+    generation: 4,
+  },
+  {
+    value: "bw",
+    text: "Black and White",
+    key: "7",
+    generation: 5,
+  },
+  {
+    value: "bw2",
+    text: "Black 2 and White 2",
+    key: "8",
+    generation: 5,
+  },
+];
