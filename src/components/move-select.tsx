f7c75e7019c02a6602a5453a06df631dcc762f03
vchho <12765624+vchho@users.noreply.github.com>
2024-07-09 15:02:07 -0700

    created move select component

---
+++ src/components/move-select.tsx
@@ -0,0 +1,110 @@
+import { useEffect, useState } from "react";
+import { Check, ChevronsUpDown } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { ScrollArea } from "./ui/scroll-area";
+import useStore from "@/store/store";
+import { PokemonDetail } from "@/types/AppState";
+import { Move } from "@/types/move";
+
+export function MoveSelect({
+  moves,
+  teamId,
+  pokemonIndex,
+  pokeDetail,
+  moveIndex,
+  moveInformation,
+  item,
+}: {
+  moves: Move[];
+  teamId: string;
+  pokemonIndex: number;
+  pokeDetail: PokemonDetail;
+  moveIndex: number;
+  moveInformation: any;
+  item?: string;
+}) {
+  const [open, setOpen] = useState(false);
+  const [setMove, setValue] = useState("");
+
+  const changeTeamMemberInfo = useStore((state) => state.changeTeamMemberInfo);
+
+  useEffect(() => {
+    if (item) {
+      const foundMove = moves.find((move) => String(move.id) === item);
+
+      if (foundMove) {
+        setValue(foundMove.name);
+      }
+    }
+  }, [item]);
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          // className="w-{90%} mx-1 justify-between"
+          className="w-full justify-between"
+        >
+          {setMove
+            ? moves.find((item) => item.name === setMove)?.name
+            : "Select Move"}
+          <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[300px] p-0">
+        <Command>
+          <CommandInput placeholder="Search Item" />
+          {/* https://github.com/shadcn-ui/ui/issues/607 */}
+          <ScrollArea className="h-[220px] overflow-auto">
+            <CommandEmpty>No move found.</CommandEmpty>
+            <CommandGroup>
+              {moves.map((move) => (
+                <CommandItem
+                  key={move.name}
+                  value={String(move.id)}
+                  onSelect={() => {
+                    setValue(move.name);
+                    changeTeamMemberInfo(teamId, pokemonIndex, {
+                      ...pokeDetail,
+                      moves: [
+                        ...moveInformation.slice(0, moveIndex),
+                        move.id,
+                        ...moveInformation.slice(moveIndex),
+                      ],
+                    });
+                    setOpen(false);
+                  }}
+                >
+                  <Check
+                    className={cn(
+                      "mr-2 h-4 w-4",
+                      setMove === move.name ? "opacity-100" : "opacity-0",
+                    )}
+                  />
+                  {move.name}
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </ScrollArea>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
