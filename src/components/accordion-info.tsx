4d311694916c35b220470f42ce313afb6327c0ca
vchho <12765624+vchho@users.noreply.github.com>
2024-07-09 15:04:31 -0700

    accordion info updated

--- src/components/accordion-info.tsx
+++ src/components/accordion-info.tsx
@@ -14,21 +14,43 @@ import ABILITIES from "@/constants/abilities";
 
 import { NatureSelect } from "./nature-select";
 import { AbilitiesSelect } from "./abilities-select";
+import { ITEMS2 } from "@/constants/items";
+import { HeldItemSelect } from "./held-item-select";
+import useStore from "@/store/store";
+import { useParams } from "react-router-dom";
+import { MoveSelect } from "./move-select";
+import { MOVES } from "@/constants/moves";
+import { useMemo } from "react";
 
 export function AccordionInfo({
   pokemonId,
   pokeDetail,
   teamId,
   orderIndex,
+  currentGeneration,
 }: {
   pokemonId: number;
   pokeDetail: PokemonDetail;
   teamId: string;
-  orderIndex?: number;
+  orderIndex: number;
+  currentGeneration?: string;
 }) {
+  const { id } = useParams() as { id: string };
   const pokemon = POKEMAP.get(pokemonId);
 
   const accordionKey = pokemon?.text! + pokemon?.value!;
+
+  const deleteTeamMember = useStore((state) => state.deleteTeamMember);
+
+  const filteredMoves = useMemo(() => {
+    const filtered = MOVES.filter((pokemon) =>
+      Number(currentGeneration) > 0
+        ? Number(pokemon.gen) <= Number(currentGeneration)
+        : true,
+    );
+
+    return filtered;
+  }, [currentGeneration]);
 
   return (
     <AccordionItem value={`${accordionKey}`}>
@@ -55,26 +77,89 @@ export function AccordionInfo({
         </div>
 
         <div className="my-4 flex"></div>
-        <NatureSelect
-          natures={NATURES}
+        {currentGeneration !== "1" && currentGeneration !== "2" ? (
+          <>
+            <NatureSelect
+              natures={NATURES}
+              teamId={teamId}
+              pokemonIndex={orderIndex}
+              nature={pokeDetail?.nature ?? ""}
+              pokeDetail={pokeDetail}
+            />
+            <AbilitiesSelect
+              abilities={ABILITIES}
+              teamId={teamId}
+              pokemonIndex={orderIndex}
+              ability={pokeDetail.ability ?? ""}
+              pokeDetail={pokeDetail}
+            />
+            <HeldItemSelect
+              items={ITEMS2}
+              teamId={teamId}
+              pokemonIndex={orderIndex}
+              item={pokeDetail.item ?? ""}
+              pokeDetail={pokeDetail}
+            />
+          </>
+        ) : null}
+        <MoveSelect
+          moves={filteredMoves}
           teamId={teamId}
-          pokemonIndex={orderIndex!}
-          nature={pokeDetail?.nature ?? ""}
+          pokemonIndex={orderIndex}
+          item={String(pokeDetail.moves[0]) ?? ""}
           pokeDetail={pokeDetail}
+          moveInformation={[
+            pokeDetail.moves[1],
+            pokeDetail.moves[2],
+            pokeDetail.moves[3],
+          ]}
+          moveIndex={0}
+        />
+        <MoveSelect
+          moves={filteredMoves}
+          teamId={teamId}
+          pokemonIndex={orderIndex}
+          item={String(pokeDetail.moves[1]) ?? ""}
+          pokeDetail={pokeDetail}
+          moveInformation={[
+            pokeDetail.moves[0],
+            pokeDetail.moves[2],
+            pokeDetail.moves[3],
+          ]}
+          moveIndex={1}
+        />
+        <MoveSelect
+          moves={filteredMoves}
+          teamId={teamId}
+          pokemonIndex={orderIndex}
+          item={String(pokeDetail.moves[2]) ?? ""}
+          pokeDetail={pokeDetail}
+          moveInformation={[
+            pokeDetail.moves[0],
+            pokeDetail.moves[1],
+            pokeDetail.moves[3],
+          ]}
+          moveIndex={2}
+        />
+        <MoveSelect
+          moves={filteredMoves}
+          teamId={teamId}
+          pokemonIndex={orderIndex}
+          item={String(pokeDetail.moves[3]) ?? ""}
+          pokeDetail={pokeDetail}
+          moveInformation={[
+            pokeDetail.moves[0],
+            pokeDetail.moves[1],
+            pokeDetail.moves[2],
+          ]}
+          moveIndex={3}
         />
 
-        <AbilitiesSelect
-          abilities={ABILITIES}
-          teamId={teamId}
-          pokemonIndex={orderIndex!}
-          nature={pokeDetail?.ability ?? ""}
-          pokeDetail={pokeDetail}
-        />
         <div className="mt-5 flex justify-center">
           <Button
             className="w-full"
             variant={"destructive"}
-            // onClick={() => handleDeleteTeamMember(id, orderIndex)}
+            onClick={() => deleteTeamMember(id, orderIndex)}
           >
             Delete
           </Button>
