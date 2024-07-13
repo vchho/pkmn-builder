43139a30497200f6b5b46bca3bec62df1ceebc42
vchho <12765624+vchho@users.noreply.github.com>
2024-07-12 22:36:23 -0700

    Added tailwind map colors

---
+++ src/constants/tailwind-colors.tsx
@@ -0,0 +1,11 @@
+type Gradientcolors = {
+  [key: string]: string;
+};
+
+export const generationColors: Gradientcolors = {
+  "1": "bg-gradient-to-b from-red-600 via-blue-600 to-yellow-300",
+  "2": "bg-gradient-to-b from-amber-200 via-slate-200 to-indigo-200",
+  "3": "bg-gradient-to-b from-indigo-600 via-rose-600 to-emerald-400",
+  "4": "bg-gradient-to-b from-sky-200 via-pink-200 to-zinc-200",
+  "5": "bg-gradient-to-b from-slate-900 to-slate-50",
+};
