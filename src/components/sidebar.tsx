881b98448b940383e693259a0d7dc0eb62c156a0
vchho <12765624+vchho@users.noreply.github.com>
2024-10-28 01:56:03 -0700

    removed

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -3,9 +3,6 @@ import { SidebarRoutes } from "./sidebar-routes";
 export const Sidebar = () => {
   return (
     <div className="flex h-full flex-col overflow-y-auto border-r bg-white shadow-sm">
-      {/* <div className="p-6"> */}
-      {/*   <Logo /> */}
-      {/* </div> */}
       <div className="flex w-full flex-col">
         <SidebarRoutes />
       </div>
